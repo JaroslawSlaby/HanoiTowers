d88c2094e239c3e530ed8ef2ff74c3bc006cc648
Jaroslaw Slaby <jaroslaw.slaby@gmail.com>
2018-05-23 11:14:30 +0200

    added input with choosing number of disks

--- src/HanoiTowers.java
+++ src/HanoiTowers.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class HanoiTowers {
 
-    private void solvePuzzle() {
+    private void solvePuzzle(int numberOfDisks) {
+        //
 
+        //
     }
 
 
     public static void main(String[] args) {
         HanoiTowers hanoiTowers = new HanoiTowers();
-        hanoiTowers.solvePuzzle();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of disks: ");
+        int numberOfDisks = input.nextInt();
+        hanoiTowers.solvePuzzle(numberOfDisks);
     }
 }
